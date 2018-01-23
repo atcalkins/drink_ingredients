365100734c38fec5dc967ec270c513c16238b36a
Tyler Calkins <atcalkins@outlook.com>
2018-01-23 12:22:58 -0500

    heroku bug work

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -18,9 +18,6 @@ Bundler.require(*Rails.groups)
 
 module ListOfIngredients
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -30,10 +27,10 @@ module ListOfIngredients
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.app_generators.scaffold_controller = :scaffold_controller
+
     config.middleware.use Rack::MethodOverride
     config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-    config.assets.initialize_on_precompile = false
   end
 end
